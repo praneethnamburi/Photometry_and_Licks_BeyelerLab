435cc3cf8d4e2f16e7592692b68e0e76dd4b405a
praneeth <praneeth@mit.edu>
2022-04-22 17:51:52 -0400

    added regression

--- beyelerlab/__init__.py
+++ beyelerlab/__init__.py
@@ -1,5 +1,11 @@
 from collections import Counter
+from copy import copy
+
 import numpy as np
+import pandas as pd
+
+import pntools as pn
+from pntools import sampled
 
 class Bout:
     def __init__(self, timestamps, th=10):
@@ -46,3 +52,71 @@ class Bout:
     
     def n_events_per_bout(self):
         return list(Counter(self.bout_classes()).values())
+
+
+class Dataset(pn.FileManager):
+    def __init__(self, base_dir, **kwargs):
+        # default parameters
+        p = copy(kwargs)
+
+        p['file_types'] = kwargs.pop('file_types', ('lick_times', 'isos', 'GCaMP'))
+        p['use_names_from'] = kwargs.pop('use_names_from', 'GCaMP')
+        p['col_selector'] = kwargs.pop('col_selector', 'Mouse') # column selector for mouse names
+        p['target_sr'] = kwargs.pop('target_sr', 30)
+        p['mouse_col_offset'] = kwargs.pop('mouse_col_offset', 11)
+        p['bout_th'] = kwargs.pop('bout_th', 10)
+
+        super().__init__(base_dir)
+        for file_type in p['file_types']:
+            self.add(file_type, f'*{file_type}*.csv')
+            assert len(self[file_type]) == 1
+        
+        self.mouse_names = get_mouse_names(self[p['use_names_from']][0], p['col_selector'])
+        self.lick_times = read_lick_file(self['lick_times'][0], self.mouse_names, p['mouse_col_offset'], p['bout_th'])
+        self.isos = read_sig_file(self['isos'][0], p['target_sr'])
+        t_min = self.isos[self.mouse_names[0]].t_start()
+        t_max = self.isos[self.mouse_names[0]].t_end()
+        for file_type in p['file_types'][2:]:
+            setattr(self, file_type, read_sig_file(self[file_type][0], p['target_sr'], t_min, t_max))
+        
+        self.params = pn.dotdict(p)
+        self.t_lim = (t_min, t_max)
+    
+    def get(self, key):
+        assert key in self.mouse_names
+        ret = pn.dotdict()
+        for file_type in self.params['file_types']:
+            ret[file_type] = getattr(self, file_type)[key]
+        return ret
+    
+    __call__ = get
+
+
+def get_mouse_names(fname, col_selector='Mouse'):
+    if isinstance(fname, str):
+        df = pd.read_csv(fname)
+    else:
+        assert isinstance(fname, pd.DataFrame)
+        df = fname
+    m_names = [x for x in df.columns.values.tolist() if col_selector in x]
+    return m_names
+
+def read_sig_file(sig_file, target_sr=30, t_min=None, t_max=None):
+    df = pd.read_csv(sig_file)
+    m_names = get_mouse_names(df)
+    t = df['Timestamp'].to_numpy()
+    m_data = {}
+    for m_name in m_names:
+        m_data[m_name] = sampled.uniform_resample(t, df[m_name].to_numpy(), target_sr, t_min, t_max)
+    return m_data
+
+def read_lick_file(lick_file, mouse_names=(1, 2, 3, 4), mouse_col_offset=11, bout_th=10):
+    df = pd.read_csv(lick_file).to_numpy()
+    t = df[:, 0]
+    m_licktimes = dict()
+    for mouse_count, mouse_name in enumerate(mouse_names):
+        m_licktimes[mouse_name] = Bout(t[df[:, mouse_col_offset+mouse_count].astype(bool)], bout_th)
+    return m_licktimes
+
+if __name__ == '__main__':
+    Dataset(r'C:\Users\prane\Downloads\PhotoM groupe 1')
